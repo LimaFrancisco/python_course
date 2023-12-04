141ffa7e9858c6ecbfaec0f9d7649f028de0e4d6
LimaFrancisco <francisco1111.fl@protonmail.com>
2023-12-04 00:24:34 -0300

    primitive types and printing variables

---
+++ aula02.py
@@ -0,0 +1,15 @@
+name = "Francisco"
+age = "25" # String
+height = 1.65 # float
+like = "Programming"
+is_male = True # bool
+
+print("My name is " + name)
+print("I am " + age + " years old")
+print("I like " + like)
+
+age = 30 # int
+
+print(f"{name} likes {like} and is {age} years old") # best form of printing variables 
+
+# primitive types: String, int, float, bool
