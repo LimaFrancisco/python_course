28cb0e6b06a573d716b5edf4e9f18e61d0be3a81
LimaFrancisco <francisco1111.fl@protonmail.com>
2023-12-06 10:53:06 -0300

    data input

---
+++ aula05.py
@@ -0,0 +1,17 @@
+# name = input("type your name: ") # data input for variable name 
+# age = int(input(f"how old are you {name}: "))
+
+# birth = 2023 - age
+
+# print(f"{name} you are born in {birth}") # data output
+
+"""
+another way to comment,
+used in long comments
+"""
+
+num1 = int(input("type a number: "))
+num2 = int(input("type another number:"))
+result = num1 + num2
+
+print(f"the result is {result}")
