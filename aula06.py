f830ba519e27a763d1d62f6b7638dab7e09aa33f
FranciscoLima <francisco1111.fl@protonmail.com>
2023-12-11 07:38:34 -0300

    workig with collections and tuples

---
+++ aula06.py
@@ -0,0 +1,35 @@
+family = ["Roger", "Cris", "Manu", "Vini","Selina"] # collection of strings
+#            0        1       2       3       4
+#           -5       -4      -3      -2      -1 
+
+#print(family) - returns all elements
+
+#print(family[0]) - returns only one element
+#print(family[-1]) - back to front
+#print(family[2:]) - starts to 2
+#print(family[2:4]) - beetwen 2 and 4
+
+#family[3] = "Roger" - change Vini for Roger
+
+#family.extend(["Fernando", "Rosania"]) - concatenate strings
+
+#family.append("Spock") - adds at the end 
+#family.insert(1, "Spock") - adds in a specific index
+#family.pop() - removes the last element
+
+#family.pop() - removes the last element
+#family.remove("Spock") - remove a specific index
+
+#family.clear() - remove all elements 
+
+#print(family.index("Manu")) - returns the first index of element
+#print(family.count("Roger")) - returns the quantity of the element
+
+#age_family = [34, 36, 13, 11, 3] - collection of integers 
+#age_family.sort() - sort collections
+#age_family.reverse() - reversing
+
+#family2 = family - copy by reference
+#family2 = family.copy() - copy of family
+
+#coordinates = (-49, -36) - immutable list
