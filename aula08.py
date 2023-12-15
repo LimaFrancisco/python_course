6c24742a683800317a03386c427c660491b5d847
FranciscoLima <francisco1111.fl@protonmail.com>
2023-12-15 13:01:57 -0300

    aula08 - if and else and logical operators

---
+++ aula08.py
@@ -0,0 +1,65 @@
+# i_am_thirsty = False
+
+# if i_am_thirsty:
+#     print("drink water")
+
+# print("life goes on")
+
+# its_cold = False 
+
+# if its_cold:
+#     print("wear a jacket")
+# else:
+#     print("wear a t-shirt")
+
+i_am_thirsty = True
+i_am_hungry = False
+
+i_am_on_a_diet = False
+
+# if i_am_thirsty or i_am_hungry:
+#     print("go to the kitchen")
+# else:
+#     print("stay watching netflix")
+
+
+# if i_am_thirsty and i_am_hungry:
+#     print("make a sandwich and coke")
+# else:
+#     print("i`m not hungry or i`m not thirsty")
+
+# if i_am_thirsty and i_am_hungry:
+#     print("make a sandwich and coke")
+# elif i_am_thirsty and not(i_am_hungry):
+#     if i_am_on_a_diet:
+#         print("drink water")
+#     else:
+#         print("drink a coke")
+# elif not(i_am_thirsty) and i_am_hungry:
+#     print("make a sandwich")
+# else:
+#     print("stay watching netflix")
+
+num1 = 50
+num2 = 23
+
+# if num1 == num2:
+#     print("num1 is the same as num2")
+# elif num1 != num2:
+#     print("num1 is the diferente from num2") 
+# elif num1 > num2:
+#     print("num1 is greater than num2")   
+# elif num1 < num2:
+#     print("num1 is smaller thar num2 ")
+
+# comparation operators 
+# or => or
+# and => and 
+# not => negation
+
+# == -> equal
+# != -> different
+# > -> grater than
+# < -> less than
+# >= -> greater equal 
+# <= -> less equal
